d116658529c9aca7247dbae8cdba3beebea7ca59
SUMYTH14 <sumitkumar741685@gmail.com>
2025-07-23 20:44:17 +0530

    Add files via upload

---
+++ TABLE_CREATION.sql
@@ -0,0 +1,38 @@
+DROP TABLE IF EXISTS Books
+CREATE TABLE Books(
+Book_ID SERIAL PRIMARY KEY,	
+Title VARCHAR(100),
+Author VARCHAR(100),
+Genre VARCHAR(50),
+Published_Year INT,
+Price NUMERIC(10,2),
+Stock INT
+);
+
+DROP TABLE IF EXISTS Customers;
+CREATE TABLE Customers(
+Customer_ID SERIAL PRIMARY KEY,
+Name VARCHAR(100),
+Email VARCHAR(100),
+Phone VARCHAR(15),
+City VARCHAR(50),
+Country VARCHAR(150)
+);
+
+DROP TABLE IF EXISTS Orders;
+CREATE TABLE Orders(
+Order_ID SERIAL PRIMARY KEY,
+Customer_ID INT REFERENCES Customers(Customer_ID),
+Book_ID INT REFERENCES Books(Book_ID),
+Order_Date DATE,
+Quantity INT,
+Total_Amount NUMERIC(10,2)
+);
+copy Customers FROM 'C:\Users\SUMYTH\Desktop\BOOKs\Customers.csv' DELIMITER ',' CSV HEADER;
+COPY Orders FROM 'C:\Users\SUMYTH\Desktop\BOOKs\Orders.csv' DELIMITER ',' CSV HEADER;
+
+SELECT * FROM Books; 
+SELECT * FROM Customers;
+SELECT * FROM Orders;
+
+
